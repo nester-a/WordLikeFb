da89067af7cf52766589ee17da29e85f430b2ee5
anesterov <anesterov@rustest.ru>
2025-02-04 18:06:40 +0300

    делаю узел title для использования в качестве заголовков

---
+++ src/WordLikeFb/WordLikeFb/Documents/Title.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace WordLikeFb.Documents
+{
+    public class Title : Paragraph
+    {
+        public Title()
+        {
+            FontWeight = FontWeights.Bold;
+        }
+    }
+}
